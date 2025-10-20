677455f994caa0893ed23c9ed84b0c01023e1fac
Laurent Couturier <laurent_couturier@connect-tech.sncf>
2025-10-20 10:26:56 +0200

    feat: :sparkles: add functional methods

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,14 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var result = 1.into(x => x + 2).into(x => x * 3);
+            Console.WriteLine(result());
+
+            var o = Optional<int>.of(5);
+            o.Match(
+                some => Console.WriteLine($"Value: {some}"),
+                () => Console.WriteLine("No value")
+            );
         }
     }
 
@@ -64,6 +72,47 @@ namespace ConsoleApp1
         public static Func<T2, TResult> Curry<T1, T2, TResult>(this Func<T1, T2, TResult> func, T1 arg1)
         {
             return (T2 arg2) => func(arg1, arg2);
+        }
+
+        public static Func<TResult> into<T1>(this T1, Func<T1, TResult> func)
+        {
+            return () => func(arg1);
+        }
+
+        public static Func<TResult> into<T1>(this Func<T1>, Func<T1, TResult> func)
+        {
+            return () => func(arg1);
+        }
+
+
+
+        public static Func<T1, TResult> memoize<T1, TResult>(this Func<T1, TResult> func)
+        where T1 : notnull
+        {
+            var cache = new ConcurrentDictionary<T1, TResult>();
+        
+            return arg1 => cache.GetOrAdd(arg1, func);
+        }
+    }
+
+
+    public class Optional<T>(T value)
+    {
+        public static Optional<T> of(T value) => new Optional<T>(value);
+        public static Optional<T> none => new Optional<T>(default!);
+
+        private readonly T _value = value;
+
+        public bool HasValue => _value != null;
+
+        public T ValueOrDefault(T defaultValue)
+        {
+            return HasValue ? _value : defaultValue;
+        }
+
+        public TResult Match<TResult>(Func<T, TResult> someFunc, Func<TResult> noneFunc)
+        {
+            return HasValue ? someFunc(_value) : noneFunc();
         }
     }
 }
